cb6e10523d71dbfe68083922fe4190ab27930d2e
junwenyu <junwenyu@uw.edu>
2021-05-19 22:27:43 -0700

    bar chart add

--- Script/bar_chart.R
+++ Script/bar_chart.R
@@ -10,21 +10,24 @@ fossil_fuel_combustion <- read.csv("data/fossil-fuel-primary-energy.csv",
 names(fossil_fuel_combustion)[1] <- "country"
 
 
-bar_chart_info <- fossil_fuel_combustion %>%
-  filter(country != "World") %>%
-  filter(Year == "1965" | Year == "1992" | Year == "2019") %>%
-  select(Year, country, Fossil.Fuels..TWh.)
+bar_chart <- function(dataset) {
+  bar_chart_info <- dataset %>%
+    filter(country != "World") %>%
+    filter(Year == "1965" | Year == "1992" | Year == "2019") %>%
+    select(Year, country, Fossil.Fuels..TWh.)
 
-ggplot(bar_chart_info) +
-  geom_col(
-    mapping = aes(x = country, y = Fossil.Fuels..TWh., fill = Year),
-    width = 0.8
-  ) +
-  scale_fill_gradient(low = "darkgray", high = "darkred") +
-  labs(
-    title = " Fossil Fuels Combustion from 1965, 1992 and 2019 by Country",
-    x = "Country",
-    y = "Fossil Fuels (TWh)"
-  ) +
-  coord_flip()
+  ggplot(bar_chart_info) +
+    geom_col(
+      mapping = aes(x = country, y = Fossil.Fuels..TWh., fill = Year),
+      width = 0.8
+    ) +
+    scale_fill_gradient(low = "darkgray", high = "darkred") +
+    labs(
+      title = " Fossil Fuels Combustion from 1965, 1992 and 2019 by Country",
+      x = "Country",
+      y = "Fossil Fuels (TWh)"
+    ) +
+    coord_flip()
+}
 
+bar_chart(fossil_fuel_combustion)
