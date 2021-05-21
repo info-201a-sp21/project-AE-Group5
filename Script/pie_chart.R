2df0d0518c2a07582ab39169bed33d7b91ae353d
mongphile <mongphi@uw.edu>
2021-05-20 23:34:18 -0700

    finished

--- Script/pie_chart.R
+++ Script/pie_chart.R
@@ -4,18 +4,25 @@ library(plotly)
 fossil_fuel_combustion <- read.csv("data/fossil-fuel-primary-energy.csv",
                                                 stringsAsFactors = FALSE)
 
-only_2019 <- fossil_fuel_combustion %>%
-  filter(Year == "2019")
 
 pie_chart <- function(dataset) {
-  fig <- plot_ly(dataset, labels = ~, values = ~1, type = "pie")
-  fig <- fig %>% layout(title = "2019 Fossil Fuel Usage Breakdown",
-                        xaxis = list(showgrid = FALSE,
-                                     zeroline = FALSE,
-                                     showticklabels = FALSE),
-                        yaxis = list(showgrid = FALSE,
-                                     zeroline = FALSE,
-                                     showticklabels = FALSE))
+  pie_chart_info <- dataset %>%
+    filter(Entity != "World") %>%
+    filter(Year == "2019") %>%
+    select(Year, Entity, Fossil.Fuels..TWh.)
+  
+  ggplot(pie_chart_info) +
+    geom_col(
+      mapping = aes(x = Entity, y = Fossil.Fuels..TWh., fill = Year),
+      width = 0.8
+    ) +
+    scale_fill_gradient(low = "gray", high = "red") +
+    labs(
+      title = "2019 Fossil Fuel Consumption Broken Down",
+      x = "Country",
+      y = "Fossil Fuels (TWh)"
+    ) +
+    coord_polar()
 }
 
 pie_plot <- pie_chart(fossil_fuel_combustion)
