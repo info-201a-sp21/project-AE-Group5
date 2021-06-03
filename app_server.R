6030def8f2b99f0eeacc13ae76cfbfa65a38b0b5
Zimo135 <zhuzimo@uw.edu>
2021-06-04 02:02:56 +0800

    putting things together

---
+++ app_server.R
@@ -0,0 +1,69 @@
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+
+fossil_fuel <- read.csv("data/fossil-fuel-primary-energy.csv")
+
+df <- read.csv("data/GlobalLandTemperaturesByCountry.csv")
+
+temperature_change <-  df %>%
+  mutate(year =
+           format(as.Date(df$dt),format = "%Y"
+           )) %>% 
+  filter(year %in% 1964:2013) %>%
+  group_by(Country) %>% 
+  filter (!duplicated(year)) %>% 
+  select(Country, year, AverageTemperature)
+
+
+create_scatter <- function(data, search) {
+  data <- data %>%
+    filter(Entity == search)
+  
+  p <- ggplot(data = data) +
+    geom_point(mapping = aes(x = Year, y = Fossil.Fuels..TWh.)) +
+    labs(title = paste0("Fossil Fuel Combustion in ", data$Entity),
+         x = "Year", y = "Fossil Fuel Combustion")
+  p <- ggplotly(p)
+  return(p)
+}
+
+
+
+server <- function(input, output) {
+  # page 1
+  output$scatterPlot <- renderPlotly(
+    return(create_scatter(fossil_fuel, input$search))
+  )
+  
+  # page 2
+  output$map <- renderPlotly({
+    
+    l <- list(color = toRGB("grey"), width = 0.2)
+    
+    g <- list(
+      showframe = FALSE,
+      showcoastlines = FALSE,
+      projection = list(type = 'Mercator')
+    )
+    
+    temperature_change <- temperature_change %>%
+      filter(year == input$select)
+    
+    fig <- plot_geo(temperature_change, locationmode = "country names") %>% 
+      add_trace(
+        z = ~AverageTemperature, color = ~AverageTemperature, colors = "Blues",
+        text = ~Country, locations = ~Country, marker = list(line = l)
+      ) %>% 
+      colorbar(title = "Average Temperature") %>% 
+      layout(
+        title = paste0("Global Average Temperature in",input$select),
+        geo = g
+      )
+    return(fig)
+  })
+  
+}
+
+
---
+++ app_ui.R
@@ -0,0 +1,107 @@
+# Introduction page
+
+intro_sidebar_content <- sidebarPanel(
+  h4("What Is Climate Change?"),
+  p("  Climate change includes both global warming driven by human-induced
+    emissions of greenhouse gases and the resulting large-scale shifts in
+    weather patterns."),
+  p("To cope with this problem, Paris Aggrement was signed by
+    nations to maintain the temperature well under 2.0 Celsius (3.6 Fahrenheit) in 2015.
+    However, the temperature is sitll higher than this value now."),
+  p("(Cited from Wikipedia)")
+  
+)
+
+intro_main_content <- mainPanel(
+  h2("Climate Change"),
+  h3("Why This Matters?"),
+  p("Environmental issues are becoming more and more severe in the present
+    world. One of the factors that cause the deterioration of the environment
+    is human activities. We want to find out how the human??s actions impact the
+    environment and what are the potential dangers it brings to the future.
+    Climate change is one of the most serious problem that needs to be solved
+    as soon as possible."),
+  h3("Research Questions:"),
+  h4("How is temperature change during these years?"),
+  h4("How does human activities impact the envrionment?")
+)
+
+intro_panel <- tabPanel(
+  "Introduction",
+  sidebarLayout(
+    intro_sidebar_content,
+    intro_main_content
+  )
+)
+
+
+# Interactive Scatter plot tab
+
+scatter_sidebar_content <- sidebarPanel(
+  textInput(
+    inputId = "search",
+    label = "Find a country (capitalize the first letter)",
+    value = "World"
+  )
+)
+
+scatter_main_content <- mainPanel(
+  p("Fossil fuel conbustion is one of the human activity that increase the
+    CO2 emssion, leading to serious envrironmental issues."),
+  p("This scatter attempts to show the trend of fossil fuel combustion change.
+    The plot is made to sort by different countries, and it can also make a
+    plot that shows the world's data (whihc is the defalt value). The
+    scatter plot demonstrates an increasing trend of fossil fuel combustion."),
+  plotlyOutput("scatterPlot"),
+  h5("Insights:"),
+  h6("- The amount of fossil fuel combustion increases."),
+  h6("- There are some fluctuations in each country, but the overall trend
+     is increasing from 1965 to 2019.")
+)
+
+scatter_panel <- tabPanel(
+  "Fossil Fuel Combustion",
+  titlePanel("Fossil Fuel Combustion"),
+  sidebarLayout(
+    scatter_sidebar_content,
+    scatter_main_content
+  )
+)
+
+
+#Interactive map design
+
+#map_design
+Map_sidebar_content1 <- sidebarPanel(
+  selectInput(
+    "select", label = "Year", 
+    choices = temperature_change$year
+  ),
+  
+)
+map_main_content <- mainPanel(
+  p("This is the interactive map which display the earth surface temperature 
+  change by comparing countries in different years.Through this map, it show us 
+  a better idea of climate trends in 50 years."),
+  plotlyOutput("map"),
+  h5("Insights:"),
+  h6("- The general global trend of earth surface temperature is increasing."),
+  h6("- The develpoed countires' earth surface temperature relatively have more 
+  increase than deloping countires.")
+)
+
+map_panel <- tabPanel(
+  "Earth Surface Temperature",
+  titlePanel("Earth Surface Temperature"),
+  sidebarLayout(
+    Map_sidebar_content1,
+    map_main_content
+  )
+)
+
+ui <- navbarPage(
+  "Climate Change",
+  intro_panel,
+  scatter_panel,
+  map_panel
+)
